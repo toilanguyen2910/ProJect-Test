224be862e34ce46cbba41bbca03c65334101de15
toilanguyen2910 <taipan27.game@gmail.com>
2026-04-09 13:12:07 +0700

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/ProJect-Test/',
   plugins: [
     react(),
     basicSsl()
